abd9a83f897a52daf8ae09a5fe3ba1bcb2ff142a
Eddie Maceda <eddiemaceda11@gmail.com>
2025-06-26 17:52:48 -0700

    Create all of the sidebar component files

---
+++ src/app/sidebar/desktop-sidebar/desktop-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-sidebar',
+  imports: [],
+  templateUrl: './desktop-sidebar.component.html',
+  styleUrl: './desktop-sidebar.component.scss'
+})
+export class DesktopSidebarComponent {
+
+}
---
+++ src/app/sidebar/tablet-sidebar/tablet-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tablet-sidebar',
+  imports: [],
+  templateUrl: './tablet-sidebar.component.html',
+  styleUrl: './tablet-sidebar.component.scss'
+})
+export class TabletSidebarComponent {
+
+}
