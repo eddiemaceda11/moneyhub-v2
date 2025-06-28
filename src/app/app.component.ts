bb5de22b2dc3e93df17118229bcd6e406d988e7d
Eddie Maceda <eddiemaceda11@gmail.com>
2025-06-27 17:33:17 -0700

    Update the component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DesktopSidebarComponent } from './sidebar/desktop-sidebar/desktop-sidebar.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [DesktopSidebarComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
