a5edb404521a5bb16db75594d4197319c1ccd076
Eddie Maceda <eddiemaceda11@gmail.com>
2025-06-28 10:01:35 -0700

    Configure and style the DesktopSidebarComponent

--- src/app/sidebar/desktop-sidebar/desktop-sidebar.component.ts
+++ src/app/sidebar/desktop-sidebar/desktop-sidebar.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-desktop-sidebar',
-  imports: [],
+  //imports: [NgClass],
   templateUrl: './desktop-sidebar.component.html',
-  styleUrl: './desktop-sidebar.component.scss'
+  styleUrl: './desktop-sidebar.component.scss',
 })
 export class DesktopSidebarComponent {
+  sidebarOpen = true;
+  @Output() sidebar = new EventEmitter();
 
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+    this.sidebar.emit(!this.sidebarOpen);
+  }
 }
