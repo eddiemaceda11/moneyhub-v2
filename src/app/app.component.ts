84fcf504750079fc5f5151940740a10d17b72785
Eddie Maceda <eddiemaceda11@gmail.com>
2025-06-28 10:03:56 -0700

    Create a toggleSidebar function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DesktopSidebarComponent } from './sidebar/desktop-sidebar/desktop-sidebar.component';
 import { MainComponent } from './main/main.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [DesktopSidebarComponent, MainComponent],
+  imports: [DesktopSidebarComponent, MainComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'moneyhub-v2';
+  sidebarOpen = true;
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
