b4ff2f6ea7bdbdeecd6ca7c9e351f90ccc01a9d3
Eddie Maceda <eddiemaceda11@gmail.com>
2025-07-03 23:42:34 -0700

    Create the DashboardSinkingFundsComponent files

---
+++ src/app/main/dashboard/components/dashboard-sinking-funds/dashboard-sinking-funds.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-sinking-funds',
+  imports: [],
+  templateUrl: './dashboard-sinking-funds.component.html',
+  styleUrl: './dashboard-sinking-funds.component.scss'
+})
+export class DashboardSinkingFundsComponent {
+
+}
