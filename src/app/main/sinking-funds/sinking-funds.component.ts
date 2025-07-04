a9d9f4bd9b6279c6f27e3e9b97a9c03ae2ca6174
Eddie Maceda <eddiemaceda11@gmail.com>
2025-07-03 23:32:59 -0700

    Create the SinkingFundsComponent files

---
+++ src/app/main/sinking-funds/sinking-funds.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sinking-funds',
+  imports: [],
+  templateUrl: './sinking-funds.component.html',
+  styleUrl: './sinking-funds.component.scss'
+})
+export class SinkingFundsComponent {
+
+}
