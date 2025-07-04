7fc743d4a711051d2d8fc144ff542f4b0e0c1ecb
Eddie Maceda <eddiemaceda11@gmail.com>
2025-07-04 00:14:10 -0700

    Export the DashboardUpcomingPaymentsComponent into separate files

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { DashboardSinkingFundsComponent } from './dashboard/components/dashboard-sinking-funds/dashboard-sinking-funds.component';
+import { DashboardTransactionsComponent } from './dashboard/components/dashboard-transactions/dashboard-transactions.component';
+import { DashboardBudgetsComponent } from './dashboard/components/dashboard-budgets/dashboard-budgets.component';
 
 @Component({
   selector: 'app-main',
-  imports: [DashboardSinkingFundsComponent],
+  imports: [
+    DashboardSinkingFundsComponent,
+    DashboardTransactionsComponent,
+    DashboardBudgetsComponent,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
