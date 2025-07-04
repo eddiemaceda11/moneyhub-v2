aca9cf4a020a4e1b72cc32847af7b509b3f4ea9c
Eddie Maceda <eddiemaceda11@gmail.com>
2025-07-03 23:45:17 -0700

    Create the DashboardUpcomingPaymentsComponent files

---
+++ src/app/main/dashboard/components/dashboard-upcoming-payments/dashboard-upcoming-payments.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-upcoming-payments',
+  imports: [],
+  templateUrl: './dashboard-upcoming-payments.component.html',
+  styleUrl: './dashboard-upcoming-payments.component.scss'
+})
+export class DashboardUpcomingPaymentsComponent {
+
+}
