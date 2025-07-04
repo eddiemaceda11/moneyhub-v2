507e15316ab9183d3f66d8649e4ba7e595227223
Eddie Maceda <eddiemaceda11@gmail.com>
2025-07-03 23:44:00 -0700

    Create the DashboardBudgets component files

---
+++ src/app/main/dashboard/components/dashboard-budgets/dashboard-budgets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-budgets',
+  imports: [],
+  templateUrl: './dashboard-budgets.component.html',
+  styleUrl: './dashboard-budgets.component.scss'
+})
+export class DashboardBudgetsComponent {
+
+}
