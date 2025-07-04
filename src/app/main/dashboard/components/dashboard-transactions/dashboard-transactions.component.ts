84aa2f14d6fe733e69866348455b70ddd92b52d7
Eddie Maceda <eddiemaceda11@gmail.com>
2025-07-03 23:41:21 -0700

    Create the DashboardTransactionsComponent files

---
+++ src/app/main/dashboard/components/dashboard-transactions/dashboard-transactions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-transactions',
+  imports: [],
+  templateUrl: './dashboard-transactions.component.html',
+  styleUrl: './dashboard-transactions.component.scss'
+})
+export class DashboardTransactionsComponent {
+
+}
