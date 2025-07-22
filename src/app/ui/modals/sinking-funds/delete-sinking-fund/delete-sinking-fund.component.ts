bded5fe547f26f1761936693f64b09dae7ac5f48
Eddie Maceda <eddiemaceda11@gmail.com>
2025-07-21 21:02:46 -0700

    Create the DeleteSinkingFund modal component

---
+++ src/app/ui/modals/sinking-funds/delete-sinking-fund/delete-sinking-fund.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-sinking-fund',
+  imports: [],
+  templateUrl: './delete-sinking-fund.component.html',
+  styleUrl: './delete-sinking-fund.component.scss'
+})
+export class DeleteSinkingFundComponent {
+
+}
