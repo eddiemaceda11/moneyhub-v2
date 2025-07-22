45f6592b3ac1f0ddcb6146d60e35e99e039acee3
Eddie Maceda <eddiemaceda11@gmail.com>
2025-07-21 21:01:58 -0700

    Extract the 'Add sinking-fund dialog into its own component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
 import { DesktopSidebarComponent } from './sidebar/desktop-sidebar/desktop-sidebar.component';
 import { MainComponent } from './main/main.component';
 import { NgClass } from '@angular/common';
+import { TransactionsService } from './services/transactions.service';
+import { AddSinkingFundComponent } from './ui/modals/sinking-funds/add-sinking-fund/add-sinking-fund.component';
+// import { ServiceService, User } from './services/service.service';
 
 @Component({
   selector: 'app-root',
-  imports: [DesktopSidebarComponent, MainComponent, NgClass],
+  imports: [
+    DesktopSidebarComponent,
+    MainComponent,
+    NgClass,
+    AddSinkingFundComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'moneyhub-v2';
   sidebarOpen = true;
+  // users!: User[];
+  transactions!: [];
+
+  // constructor(private transactionsService: TransactionsService) {}
 
   toggleSidebar() {
     this.sidebarOpen = !this.sidebarOpen;
   }
+
+  toggleModal() {}
+
+  // ngOnInit() {
+  //   this.transactionsService
+  //     .getTransactions()
+  //     .subscribe((data) => console.log(data));
+
+  // this.serviceService.getUsers().subscribe((data) => console.log(data));
+  // this.serviceService.getUserById(1).subscribe((data) => console.log(data));
+  // }
 }
