b339980c28a3ac225e667e39324882887a453edc
Eddie Maceda <eddiemaceda11@gmail.com>
2025-07-31 13:45:27 -0700

    Import the DashboardUpcomingPayments component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DashboardSinkingFundsComponent } from './dashboard/components/dashboard-sinking-funds/dashboard-sinking-funds.component';
 import { DashboardTransactionsComponent } from './dashboard/components/dashboard-transactions/dashboard-transactions.component';
 import { DashboardBudgetsComponent } from './dashboard/components/dashboard-budgets/dashboard-budgets.component';
+import { DashboardUpcomingPaymentsComponent } from './dashboard/components/dashboard-upcoming-payments/dashboard-upcoming-payments.component';
 
 @Component({
   selector: 'app-main',
@@ -9,6 +10,7 @@ import { DashboardBudgetsComponent } from './dashboard/components/dashboard-budg
     DashboardSinkingFundsComponent,
     DashboardTransactionsComponent,
     DashboardBudgetsComponent,
+    DashboardUpcomingPaymentsComponent,
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
