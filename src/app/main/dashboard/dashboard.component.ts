83ecb1ca15f3e0075fc0647f60938fd6eb3e3e70
Eddie Maceda <eddiemaceda11@gmail.com>
2025-08-12 22:35:55 -0700

    Export the code from the 'main' files to the 'dashboard' files

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { DashboardSinkingFundsComponent } from './components/dashboard-sinking-funds/dashboard-sinking-funds.component';
+import { DashboardTransactionsComponent } from './components/dashboard-transactions/dashboard-transactions.component';
+import { DashboardBudgetsComponent } from './components/dashboard-budgets/dashboard-budgets.component';
+import { DashboardUpcomingPaymentsComponent } from './components/dashboard-upcoming-payments/dashboard-upcoming-payments.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    DashboardSinkingFundsComponent,
+    DashboardTransactionsComponent,
+    DashboardBudgetsComponent,
+    DashboardUpcomingPaymentsComponent,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,15 +3,11 @@ import { DashboardSinkingFundsComponent } from './dashboard/components/dashboard
 import { DashboardTransactionsComponent } from './dashboard/components/dashboard-transactions/dashboard-transactions.component';
 import { DashboardBudgetsComponent } from './dashboard/components/dashboard-budgets/dashboard-budgets.component';
 import { DashboardUpcomingPaymentsComponent } from './dashboard/components/dashboard-upcoming-payments/dashboard-upcoming-payments.component';
+import { RouterOutlet } from '../../../node_modules/@angular/router/router_module.d-Bx9ArA6K';
 
 @Component({
   selector: 'app-main',
-  imports: [
-    DashboardSinkingFundsComponent,
-    DashboardTransactionsComponent,
-    DashboardBudgetsComponent,
-    DashboardUpcomingPaymentsComponent,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
