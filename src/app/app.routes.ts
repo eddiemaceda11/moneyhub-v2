1d582806e4d5e0630563891870b517bac242f547
Eddie Maceda <eddiemaceda11@gmail.com>
2025-08-12 22:27:09 -0700

    Configure the initial routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './main/dashboard/dashboard.component';
+import { TransactionsComponent } from './main/transactions/transactions.component';
+import { BudgetsComponent } from './main/budgets/budgets.component';
+import { SinkingFundsComponent } from './main/sinking-funds/sinking-funds.component';
+import { UpcomingBillsComponent } from './main/upcoming-bills/upcoming-bills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'transactions',
+    component: TransactionsComponent,
+  },
+  {
+    path: 'budgets',
+    component: BudgetsComponent,
+  },
+  {
+    path: 'sinking-funds',
+    component: SinkingFundsComponent,
+  },
+  {
+    path: 'recurring',
+    component: UpcomingBillsComponent,
+  },
+];
