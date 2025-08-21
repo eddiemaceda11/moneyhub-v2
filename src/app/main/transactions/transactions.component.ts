af43b0c5f9be7dd49bce70ab66ebb00ae406c045
Eddie Maceda <eddiemaceda11@gmail.com>
2025-08-21 13:43:48 -0700

    Set up initial functions and variables for the Transactions component

--- src/app/main/transactions/transactions.component.ts
+++ src/app/main/transactions/transactions.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { transactions } from './transactions';
+
+type Pagination = {
+  start: number;
+  end: number;
+};
 
 @Component({
   selector: 'app-transactions',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './transactions.component.html',
-  styleUrl: './transactions.component.scss'
+  styleUrl: './transactions.component.scss',
 })
 export class TransactionsComponent {
+  transactionSearchValue: string = '';
+  sortOption = 'lowest';
+  category = 'all';
+  currentPagination: Pagination = { start: 0, end: 10 };
+  filteredTransactions = transactions;
 
+  formatCurrency(value: string | number) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(Number(value));
+  }
+
+  changeSortByOption(event: Event) {
+    const select = event.target as HTMLSelectElement;
+    this.sortOption = select.value;
+    this.setPagination();
+  }
+
+  changeCategoryOption(event: Event) {
+    const select = event.target as HTMLSelectElement;
+    this.category = select.value;
+    this.setPagination();
+  }
+
+  setPagination() {
+    this.currentPagination = { start: 0, end: 10 };
+  }
 }
